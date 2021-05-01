1335a5d1c383ee95acb2802fcbf89ca1dd1018bc
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-05-01 22:20:33 +0000

    [Add]いいね機能実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
 	belongs_to :user
 	has_many :favorites, dependent: :destroy
 
-	def favorite_by?(user)
+	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
 	end
 
