7a6fcea68e9415d1d572f243a5bcc7bb4afe8fe1
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-05-03 02:51:10 +0000

    [Add]コード修正,リンク追加,自分にはフォローしないようにする

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,23 +6,23 @@ class RelationshipsController < ApplicationController
     current_user.follow(@user)
     redirect_to request.referer
   end
-  
+
   def destroy
     current_user.unfollow(@user)
-    redirect_to request.refere
+    redirect_to request.referer
   end
-  
+
   # フォロー・フォロワー一覧を表示する
   def followings
     @users = @user.followings
   end
-  
+
   def followers
     @users = @user.followers
   end
-  
+
   private
-  
+
   def set_user
     @user = User.find(params[:user_id])
   end
