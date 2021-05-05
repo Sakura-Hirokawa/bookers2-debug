30b828c7da2e9d726321276a1e66bab42ea9a644
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-05-05 02:16:49 +0000

    [Add]課題６

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,27 @@
 class FavoritesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_book
+  
   def create
-    @book = Book.find(params[:book_id])
-    favorite = @book.favorites.new(user_id: current_user.id)
-    favorite.save
-    redirect_back(fallback_location: root_path)
+    # favorite = @book.favorites.new(user_id: current_user.id)
+    @favorite = current_user.favorites.new(book_id: @book.id)
+    @favorite.save
   end
 
   def destroy
+    @favorite = current_user.favorites.find_by(book_id: @book.id)
+    @favorite.destroy
+    # favorite = @book.favorites.find_by(user_id: current_user.id)
+    # favorite.destroy
+  end
+  
+  private
+  
+  def set_book
     @book = Book.find(params[:book_id])
-    favorite = @book.favorites.find_by(user_id: current_user.id)
-    favorite.destroy
-    redirect_back(fallback_location: root_path)
+  end
+
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :book_id)
   end
 end
