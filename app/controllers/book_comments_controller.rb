da8d14a1304e0b25d42fbd8a20c342dd9c1576aa
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-05-05 11:29:56 +0000

    [Fix]課題６解答参照してコード修正（いいね機能）２かいめ

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,14 +5,18 @@ class BookCommentsController < ApplicationController
     @book_comment = BookComment.new(book_comment_params)
     @book_comment.book_id = @book.id
     @book_comment.user_id = current_user.id
-    @book_comment.save
-
+    unless @book_comment.save
+      redirect_to book_path(@book.id)
+    else
+      render 'books/show'
+    end
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     book_comment = @book.book_comments.find(params[:id])
     book_comment.destroy
+    redirect_to request.referer
   end
 
   private
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,14 +6,11 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book_new = Book.new
     @user = @book.user
-    @book_comment = BookComment.new
-    @favorite = Favorite.new
   end
 
   def index
     @book = Book.new
     @books = Book.all
-    @favorite = Favorite.new
   end
 
   def create
