ea5dc4f27b40517e2b2adccf18941cac2cad1e5e
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-05-09 22:25:52 +0000

    [Add]チャット画面表示

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,15 +1,18 @@
 class ChatsController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  before_action :authenticate_user!
 
   def create
-    if UserRoom.where(user_id: current_user.id, room_id: params[:chat][:room_id]).present?
-      @message= Chat.create(params.require(:chat).permit(:user_id, :message, :room_id)).merge(user_id: current_user.id)
+    room = Room.find(params[:room_id])
+    if UserRoom.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
+      current_user.chats.create(chat_params)
+      @chats = room.chats
+      # @message = Chat.create(params.require(:chat).permit(:user_id, :message, :room_id).merge(user_id: current_user.id))
     else
-      flash[:alert] = "メッセージ送信に失敗しました"
+      redirect_back(fallback_location: root_path)
+      # redirect_to "/chats/#{@chat.room_id}"
     end
-    redirect_to "/chats/#{@message.room_id}"
   end
-  
+
   def show
     @room = Room.find(params[:id])
     if UserRoom.where(user_id: current_user.id, room_id: @room.id).present?
@@ -20,4 +23,11 @@ class ChatsController < ApplicationController
       redirect_back(fallback_location: root_path)
     end
   end
+  
+  private
+  
+  def chat_params
+    params.require(:chat).permit(:message).merge(room_id: params[:room_id])
+  end
+  
 end
