9e22b3fbfc2549ae4774c9d52341e06c14c90522
Sakura-Hirokawa <sk.hirokawa26@gmail.com>
2021-05-09 13:43:04 +0000

    [Add]chats

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,7 +1,23 @@
 class ChatsController < ApplicationController
+  before_action :authenticate_user!, only: [:create]
+
   def create
+    if UserRoom.where(user_id: current_user.id, room_id: params[:chat][:room_id]).present?
+      @message= Chat.create(params.require(:chat).permit(:user_id, :message, :room_id)).merge(user_id: current_user.id)
+    else
+      flash[:alert] = "メッセージ送信に失敗しました"
+    end
+    redirect_to "/chats/#{@message.room_id}"
   end
   
   def show
+    @room = Room.find(params[:id])
+    if UserRoom.where(user_id: current_user.id, room_id: @room.id).present?
+      @messages = @room.chats
+      @message = Chat.new
+      @entries = @room.user_rooms
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
